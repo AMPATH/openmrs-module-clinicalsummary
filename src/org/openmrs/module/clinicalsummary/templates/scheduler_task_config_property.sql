0fa20df9e9ea2124eb1a3920ff1c19d267ba6fad
Nyoman Ribeka <nyoman@openmrs.org>
2011-08-11 20:06:11 +0000

    clinicalsummary: prunning old code

--- src/org/openmrs/module/clinicalsummary/templates/scheduler_task_config_property.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `scheduler_task_config_property` (name, value) VALUES ('Location Group List', 'Clinical Summary Group 1,Clinical Summary Group 2,Clinical Summary Group 3,Clinical Summary Group 4');
-INSERT INTO `scheduler_task_config_property` (name, value) VALUES ('Clinical Summary Group 4', '8 64 75 76 74 19 55 26 27 81 83 79 78 20 23');
-INSERT INTO `scheduler_task_config_property` (name, value) VALUES ('Clinical Summary Group 1', '1 71 72 13 28 77 14 80 56 58 15 73 70');
-INSERT INTO `scheduler_task_config_property` (name, value) VALUES ('Clinical Summary Group 3', '5 63 6 85 12 65 11 82 30 57 10 9 66 67 17 18 68 31 69');
-INSERT INTO `scheduler_task_config_property` (name, value) VALUES ('Clinical Summary Group 2', '2 3 59 60 4 61 62 54 7 24 25');
